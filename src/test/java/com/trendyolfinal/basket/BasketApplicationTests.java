8017d9e4706e46918773223923f3a6340ace53f9
alprsln <alpars44@hotmail.com>
2021-08-14 20:40:31 +0300

    Initial commit

---
+++ src/test/java/com/trendyolfinal/basket/BasketApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.trendyolfinal.basket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
